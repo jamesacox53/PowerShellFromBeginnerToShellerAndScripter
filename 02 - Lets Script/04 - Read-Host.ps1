71cc4789a70d41b6949b875b607cd8927b8472ba
jamesacox53 <jamesacox53@gmail.com>
2024-05-15 17:48:24 +0100

    Completed M2 : Read-Host to learn dealing with data

---
+++ 02 - Lets Script/04 - Read-Host.ps1
@@ -0,0 +1,9 @@
+﻿"Please input your First name:"
+
+[String] $FirstName = Read-Host
+
+"Please input your Last name:"
+
+[String] $LastName = Read-Host
+
+"Your Full name is " + $FirstName + " " + $LastName
