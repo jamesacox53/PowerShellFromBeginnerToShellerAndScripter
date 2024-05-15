133e9fa583a6c6bf50570e3839789bd647f387e8
jamesacox53 <jamesacox53@gmail.com>
2024-05-15 17:56:56 +0100

    Completed M2: How operators operate

---
+++ 02 - Lets Script/05 - HowOperatorsOperate.ps1
@@ -0,0 +1,11 @@
+﻿"Please input your First number:"
+
+[Int] $Number1 = Read-Host
+
+"Please input your Second number:"
+
+[Int] $Number2 = Read-Host
+
+$Product = $Number1 * $Number2
+
+"The product of " + $Number1 + " and " + $Number2 + " is " + $Product
