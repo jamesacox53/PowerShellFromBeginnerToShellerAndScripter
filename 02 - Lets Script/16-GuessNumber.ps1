5e33d4b2dcf822a8d15906332bf4c676b56ab81e
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 17:38:01 +0100

    Completed M2 : Shall we play a game?? Guess my number part 1

---
+++ 02 - Lets Script/16-GuessNumber.ps1
@@ -0,0 +1,14 @@
+﻿'Guess the number 1 or 2'
+[int] $Guess = Read-Host
+
+$NumberToGuess = Get-Random -Minimum 1 -Maximum 3
+
+if ($Guess -eq $NumberToGuess) {
+    'You won, it was ' + $NumberToGuess
+
+} else {
+    'You lost, sorry it was ' + $NumberToGuess
+}
+
+''
+'Game Over'
