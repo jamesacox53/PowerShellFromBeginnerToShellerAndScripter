11834453c0b66d1e66d157de00add7c989daa49a
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 11:25:21 +0100

    Completed M2 : Lab 1 Challenge

---
+++ 02 - Lets Script/Labs/Lab01.ps1
@@ -0,0 +1,16 @@
+﻿# 1.
+'PowerShell'
+'PowerShell'
+'PowerShell'
+
+# 2.
+'What is your name?'
+$Name = Read-Host
+
+'Welcome ' + $Name
+
+# 3.
+'Please input the name of the service you want to see on screen:'
+$ServiceName = Read-Host
+
+Get-Service -Name $ServiceName
