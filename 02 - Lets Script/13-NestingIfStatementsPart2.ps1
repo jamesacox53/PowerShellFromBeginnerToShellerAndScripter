7ab126ee65886301c298f22473f721069fddef8b
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 15:01:53 +0100

    Completed M2 : Nesting our if statement part 2

---
+++ 02 - Lets Script/13-NestingIfStatementsPart2.ps1
@@ -0,0 +1,33 @@
+﻿'Please input the name of a user:'
+$UserName = Read-Host
+
+$UserObject = Get-LocalUser -Name $UserName
+
+$UserObject
+
+if ($UserObject.Enabled -eq $True) {
+    'Do you want to disable User ' + $UserObject.Name + '? y/n'
+    $Answer = Read-Host
+
+    if ($Answer -eq 'y') {
+        $UserObject | Disable-LocalUser
+
+    } else {
+        'Ok, we are not going to disable ' + $UserObject.Name
+    }
+
+} else {
+    'Do you want to enable User ' + $UserObject.Name + '? y/n'
+    $Answer = Read-Host
+
+    if ($Answer -eq 'y') {
+        $UserObject | Enable-LocalUser
+
+    } else {
+        'Ok, we are not going to enable ' + $UserObject.Name
+    }
+}
+
+$UserObject
+
+Get-LocalUser -Name $UserName
