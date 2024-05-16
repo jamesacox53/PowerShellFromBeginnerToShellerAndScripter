582c713ff7f3b88594a02cba0355ecbaa4264d6b
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 15:40:44 +0100

    Completed M2 : Lab 2 Solution

---
+++ 02 - Lets Script/Labs/Lab02/Lab02Part1.ps1
@@ -0,0 +1,18 @@
+﻿'Name the Service you want to Stop/Start:'
+$Name = Read-Host
+
+$ServiceObject = Get-Service -Name $Name
+
+'The Service named ' + $ServiceObject.Name + ' has the status ' + $ServiceObject.Status
+
+if ($ServiceObject.Status -eq 'Running') {
+    $ServiceObject | Stop-Service
+
+} elseif ($ServiceObject.Status -eq 'Stopped') {
+    $ServiceObject | Start-Service
+
+}
+
+$ServiceObject = Get-Service -Name $ServiceObject.Name
+
+'The Service named ' + $ServiceObject.Name + ' has the status ' + $ServiceObject.Status
---
+++ 02 - Lets Script/Labs/Lab02/Lab02Part2.ps1
@@ -0,0 +1,29 @@
+﻿'Name the Service you want to Stop/Start:'
+$Name = Read-Host
+
+$ServiceObject = Get-Service -Name $Name
+
+'The Service named ' + $ServiceObject.Name + ' has the status ' + $ServiceObject.Status
+
+if ($ServiceObject.Status -eq 'Running') {
+    'Do you want to stop the ' + $ServiceObject.Name + ' Service? y/n'
+    $Answer = Read-Host
+    
+    if ($Answer -eq 'y') {
+        $ServiceObject | Stop-Service
+    
+    }
+
+} elseif ($ServiceObject.Status -eq 'Stopped') {
+    'Do you want to start the ' + $ServiceObject.Name + ' Service? y/n'
+    $Answer = Read-Host
+    
+    if ($Answer -eq 'y') {
+        $ServiceObject | Start-Service
+    
+    }
+}
+
+$ServiceObject = Get-Service -Name $ServiceObject.Name
+
+'The Service named ' + $ServiceObject.Name + ' has the status ' + $ServiceObject.Status
