51ee2cdfff8319dd48e5058dea5daa36cbbe1cbf
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 11:56:24 +0100

    Completed M2 : Objects, objects objects!

---
+++ 02 - Lets Script/06-Objects.ps1
@@ -0,0 +1,14 @@
+﻿$ServiceName = Get-Service -Name 'spooler'
+$ServiceName | Get-Member | Out-Host
+
+$ServiceName.Name
+$ServiceName.DisplayName
+
+'The Service with the name ' + $ServiceName.Name + ' has the status ' + $ServiceName.Status
+
+"Please input the Service Name you want to show:"
+$UserInput = Read-Host
+
+$ServiceName2 = Get-Service -Name $UserInput
+
+'The Service with the name ' + $ServiceName2.Name + ' has the status ' + $ServiceName2.Status
