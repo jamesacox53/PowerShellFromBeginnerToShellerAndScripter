b564cc46b76fb6d7d60ae3b1dc12b7c6d4317654
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 17:21:12 +0100

    Completed M2 : Logical Operators

---
+++ 02 - Lets Script/15-LogicalOperators.ps1
@@ -0,0 +1,15 @@
+﻿$Number1 = 1
+$Number2 = 1
+$Number3 = 3
+
+if (($Number1 -eq $Number2) -and ($Number1 -eq $Number3)) {
+    'And'
+}
+
+if (($Number1 -eq $Number2) -or ($Number1 -eq $Number3)) {
+    'Or'
+}
+
+if (-not ($Number1 -eq $Number2)) {
+    'Not'
+}
