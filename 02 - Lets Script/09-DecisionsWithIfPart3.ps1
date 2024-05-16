1070c854732904c46b05aa0f49b574bb11b160c5
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 12:44:40 +0100

    Completed M2 : Decisions with if Part 3

---
+++ 02 - Lets Script/09-DecisionsWithIfPart3.ps1
@@ -0,0 +1,11 @@
+﻿'Please input the name of a user:'
+$UserName = Read-Host
+
+$UserObject = Get-LocalUser -Name $UserName
+
+if ($UserObject.Enabled -eq $True) {
+    $UserObject | Disable-LocalUser
+
+} else {
+    $UserObject | Enable-LocalUser
+}
