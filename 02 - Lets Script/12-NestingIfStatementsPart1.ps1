1aeb30a8c3e52c802e45fc3112b3c6a66f331a22
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 14:44:42 +0100

    Completed M2 : Nesting our if statement part 1

---
+++ 02 - Lets Script/12-NestingIfStatementsPart1.ps1
@@ -0,0 +1,32 @@
+﻿'Input the day and I will tell you what to eat:'
+
+$Day = Read-Host
+
+if($Day -eq 'Monday') {
+    'Are you sure you want to know what you will eat on Monday? Choose y/n'
+    $Answer = Read-Host
+
+    if ($Answer -eq 'y') {
+        'On Monday you eat Bami Goreng'
+    
+    } else {
+        'Ok I am not going to show that to you.'
+    }
+
+} elseif ($Day -eq 'Tuesday') {
+    'Are you sure you want to know what you will eat on Tuesday? Choose y/n'
+    $Answer = Read-Host
+
+    if ($Answer -eq 'y') {
+        'On Tuesday you eat Rendang'
+    
+    } else {
+        'Ok I am not going to show that to you.'
+    }
+
+} elseif($Day -eq 'Wednesday') {
+    'On Wednesday you eat Nasi Goreng'
+
+} else {
+    'I do not care.'
+}
