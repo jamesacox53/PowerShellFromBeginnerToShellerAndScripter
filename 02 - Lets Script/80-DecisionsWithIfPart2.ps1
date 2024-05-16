e2b4e4d8502eaeebf4ffee8e1fa7a2a10289d3a5
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 12:26:37 +0100

    Completed M2 : Decisions with if Part 2

---
+++ 02 - Lets Script/80-DecisionsWithIfPart2.ps1
@@ -0,0 +1,16 @@
+﻿'Input the day and I will tell you what to eat:'
+
+$Day = Read-Host
+
+if($Day -eq 'Monday') {
+    'On Monday you eat Bami Goreng'
+
+} elseif ($Day -eq 'Tuesday') {
+    'On Tuesday you eat Rendang'
+
+} elseif($Day -eq 'Wednesday') {
+    'On Wednesday you eat Nasi Goreng'
+
+} else {
+    'I do not care.'
+}
