272a0205eec4643596f800d236cffb239fb62f45
jamesacox53 <jamesacox53@gmail.com>
2024-05-16 18:00:17 +0100

    Completed M2 : Looping with the While loop part 2

---
+++ 02 - Lets Script/18-WhileLoopPart2.ps1
@@ -0,0 +1,13 @@
+﻿'Pick the Service:'
+$ServiceName = Read-Host
+$ServiceObject = Get-Service -Name $ServiceName
+
+while($ServiceObject.Status -eq 'Stopped') {
+    $ServiceObject.Name + ' has the status ' + $ServiceObject.Status + ', waiting to start'
+
+    $ServiceObject = Get-Service -Name $ServiceObject.Name
+
+    sleep -Seconds 1
+}
+
+'Ok Service with name ' + $ServiceObject.Name + ' has Status ' + $ServiceObject.Status
