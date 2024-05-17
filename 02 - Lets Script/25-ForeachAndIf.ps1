09f3ff10e85de571918d2a1e0514c27955f7856d
jamesacox53 <jamesacox53@gmail.com>
2024-05-17 12:44:59 +0100

    Completed M2 : One two three combo with Foreach and if (Just not that violent..)

---
+++ 02 - Lets Script/25-ForeachAndIf.ps1
@@ -0,0 +1,12 @@
+﻿$LocalUsers = Get-LocalUser
+
+foreach($LocalUser in $LocalUsers) {
+    if ($LocalUser.Enabled -eq $True) {
+        Set-LocalUser -Name $LocalUser.Name -Description 'Enabled account'
+    
+    } else {
+        Set-LocalUser -Name $LocalUser.Name -Description 'Disabled account'
+    }
+}
+
+Get-LocalUser
