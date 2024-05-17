5c5fa27632d8e87fcd7d3463280fcb8f6f92bf19
jamesacox53 <jamesacox53@gmail.com>
2024-05-17 16:00:17 +0100

    Completed M2 : Getting text input

---
+++ 02 - Lets Script/28-GettingTextInput/28-GettingTextInput.ps1
@@ -0,0 +1,8 @@
+﻿$UsersToCreate = Get-Content -Path '.\users.txt'
+
+$Password = ConvertTo-SecureString -String 'Pa55w.rd' -AsPlainText -Force
+
+foreach($UserName in $UsersToCreate) {
+    New-LocalUser -Name $UserName -Password $Password
+
+}
