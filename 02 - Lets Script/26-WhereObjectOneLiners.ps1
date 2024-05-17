6876440f4338ef4480a27ee1bf3f8f77340fc623
jamesacox53 <jamesacox53@gmail.com>
2024-05-17 12:52:17 +0100

    Completed M2 : In case you didn't get my two Where-Objects One-Liners...

---
+++ 02 - Lets Script/26-WhereObjectOneLiners.ps1
@@ -0,0 +1,3 @@
+﻿Get-LocalUser | Where-Object {$PSItem.Enabled -eq $True} | Set-LocalUser -Description 'Enabled account'
+
+Get-LocalUser | Where-Object {$PSItem.Enabled -eq $False} | Set-LocalUser -Description 'Disabled account'
