5250044d6e7cef1bbf35f2840d3ec4959ed40bb3
jamesacox53 <jamesacox53@gmail.com>
2024-05-17 10:49:43 +0100

    Completed M2 : Shall we play a game?? Guess my number part 3

---
+++ 02 - Lets Script/20-GuessNumber3.ps1
@@ -0,0 +1,20 @@
+﻿$NumberToGuess = Get-Random -Minimum 1 -Maximum 6
+
+'Please guess the number:'
+[int] $Guess = Read-Host
+
+while ($Guess -ne $NumberToGuess) {
+    if ($Guess -lt $NumberToGuess) {
+        'Guess again, your number ' + $Guess + ' is too low.'
+
+    } else {
+        'Guess again, your number ' + $Guess + ' is too high.'
+    }
+    
+    'Please guess the number:'
+    $Guess = Read-Host
+} 
+
+'You won, it was ' + $NumberToGuess
+''
+'Game Over'
