d346dafd45e1f346ac568293da1fc0c8dfacc5e6
jamesacox53 <jamesacox53@gmail.com>
2024-05-17 10:30:08 +0100

    Completed M2 : Shall we play a game?? Guess my number part 2

---
+++ 02 - Lets Script/19-GuessNumber2.ps1
@@ -0,0 +1,15 @@
+﻿$NumberToGuess = Get-Random -Minimum 1 -Maximum 6
+
+'Please guess the number:'
+[int] $Guess = Read-Host
+
+while ($Guess -ne $NumberToGuess) {
+    "Guess again, that's not the number."
+    
+    'Please guess the number:'
+    $Guess = Read-Host
+} 
+
+'You won, it was ' + $NumberToGuess
+''
+'Game Over'
