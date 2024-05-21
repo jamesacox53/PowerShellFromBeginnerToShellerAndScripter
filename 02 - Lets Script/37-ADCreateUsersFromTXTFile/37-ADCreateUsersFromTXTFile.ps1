4fcf481b4b7ad17a5bb4ea5dc22dac667e290b56
jamesacox53 <jamesacox53@gmail.com>
2024-05-21 17:54:38 +0100

    Completed M2 : Creating Active Directory users from a simple text file

---
+++ 02 - Lets Script/37-ADCreateUsersFromTXTFile/37-ADCreateUsersFromTXTFile.ps1
@@ -0,0 +1,8 @@
+﻿$Users = Get-Content -Path '.\users.txt'
+
+$SecureStr = ConvertTo-SecureString -String 'Pa55w.rd' -AsPlainText -Force
+$OU = 'OU=Test,DC=root,dc=local'
+
+foreach($User in $Users) {
+    New-AdUser -Name $User -AccountPassword $SecureStr -Enabled $True -Path $OU
+}
