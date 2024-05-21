fa4561bdc88c123394d014e0c09af1e315a57c51
jamesacox53 <jamesacox53@gmail.com>
2024-05-21 12:03:34 +0100

    Completed M2 : Using a CSV file to actually create something!

---
+++ 02 - Lets Script/31-UsingCSVToCreate/31-UsingCSVToCreate.ps1
@@ -0,0 +1,7 @@
+﻿$UsersCSVArr = Import-Csv -Path '.\users.csv'
+
+foreach($UserCSV in $UsersCSVArr) {
+    $Password = ConvertTo-SecureString -String $UserCSV.PASSWORDOFUSER -AsPlainText -Force
+    
+    New-LocalUser -Name $UserCSV.LOGONNAME -Password $Password -Description $UserCSV.DESCRIPTION
+}
