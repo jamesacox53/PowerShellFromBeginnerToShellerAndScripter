644724d857bc19ef54a97ada369b49ecb1c3cac7
jamesacox53 <jamesacox53@gmail.com>
2024-05-21 16:17:09 +0100

    Completed M2 : Moving to Active Directory and setting AD user properties

---
+++ 02 - Lets Script/34-ADSetProperties/34-ADSetProperties.ps1
@@ -0,0 +1,7 @@
+﻿Set-ADUser -Identity 'SAM Account Name' -UserPrincipalName 'test4@root.local'
+
+Get-ADUser -Identity 'SAM Account Name'
+
+Get-ADUser -Identity 'SAM Account Name' | Set-ADUser -Department 'Sales'
+
+Get-ADUser -Identity 'SAM Account Name' -Properties *
