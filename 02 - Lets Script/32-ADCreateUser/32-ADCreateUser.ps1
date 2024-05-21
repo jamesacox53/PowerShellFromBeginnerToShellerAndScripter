15074127d4708196aa99e52780980c41ed37411b
jamesacox53 <jamesacox53@gmail.com>
2024-05-21 15:25:44 +0100

    Completed M2 : Moving to Active Directory and creating a user

---
+++ 02 - Lets Script/32-ADCreateUser/32-ADCreateUser.ps1
@@ -0,0 +1,8 @@
+﻿$SecureStr = ConvertTo-SecureString -String 'Password' -AsPlainText -Force
+
+# LDAP Path
+$LDAPPathStr = 'OU=test,DC=root,dc=local'
+
+New-ASUser -Name 'test' -AccountPassword $SecureStr -Enabled $True -Path $LDAPPathStr
+
+Get-ADUser -Identity 'test'
