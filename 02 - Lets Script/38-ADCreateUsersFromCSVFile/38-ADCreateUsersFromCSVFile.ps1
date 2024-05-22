21e9a968135b852c15c5be52acc84c31b9aaf917
jamesacox53 <jamesacox53@gmail.com>
2024-05-22 10:52:12 +0100

    Completed M2 : Creating Active Directory users from a CSV file

---
+++ 02 - Lets Script/38-ADCreateUsersFromCSVFile/38-ADCreateUsersFromCSVFile.ps1
@@ -0,0 +1,15 @@
+﻿$UsersCSV = Import-Csv -Path '.\users.csv'
+
+New-ADOrganizationalUnit -Name 'sales'
+
+New-ADOrganizationalUnit -Name 'support'
+
+New-ADOrganizationalUnit -Name 'managers'
+
+
+foreach($UserObj in $Users) {
+    $SecurePasswordStr = ConvertTo-SecureString -String $UserObj.UserPassword -AsPlainText -Force
+    $OU = 'OU=' + $UserObj.Department + ',DC=Root,DC=LOCAL'
+    
+    New-AdUser -Name $UserObj.Name -AccountPassword $SecurePasswordStr -Enabled $True -Path $OU -UserPrincipalName $UserObj.UPN -Department $UserObj.Department
+}
