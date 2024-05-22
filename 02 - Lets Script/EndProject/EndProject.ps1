5a2608027015abfa12e989c0472456184aa9e154
jamesacox53 <jamesacox53@gmail.com>
2024-05-22 14:31:10 +0100

    Completed M2 : End Project Challenge

---
+++ 02 - Lets Script/EndProject/EndProject.ps1
@@ -0,0 +1,30 @@
+﻿$SrcPathStr = 'C:\Source'
+$DestPathStr = 'C:\Dest'
+
+$DoesDestExist = Test-Path -Path $DestPathStr
+
+if ($DoesDestExist -eq $False) {
+    New-Item -Path $DestPathStr -ItemType 'Directory'
+}
+
+# Copy Images ------------------------------------
+$ImagesPathStr = $DestPathStr + '\Images'
+$DoesImagesFolderExist = Test-Path -Path $ImagesPathStr
+
+if ($DoesImagesFolderExist -eq $False) {
+    New-Item -Path $ImagesPathStr -ItemType 'directory'
+}
+
+$Images = Get-ChildItem -Path $SrcPathStr | Where-Object {$PSItem.Extension -eq '.bmp'}
+$Images | Copy-Item -Destination $ImagesPathStr
+
+# Copy Docs --------------------------------------
+$DocsPathStr = $DestPathStr + '\Docs'
+$DoesDocsFolderExist = Test-Path -Path $DocsPathStr
+
+if ($DoesDocsFolderExist -eq $False) {
+    New-Item -Path $DocsPathStr -ItemType 'directory'
+}
+
+$Docs = Get-ChildItem -Path $SrcPathStr | Where-Object {$PSItem.Extension -eq '.txt' -or $PSItem.Extension -eq '.docx'}
+$Docs | Copy-Item -Destination $DocsPathStr
